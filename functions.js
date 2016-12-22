4954fac02df467846ccf7f4694d286d84e5695d0
jamieomaguire <jamieolivermaguire@googlemail.com>
2016-12-22 20:56:34 +0000

    init commit

---
+++ functions.js
@@ -0,0 +1,9 @@
+function openNav(){
+  document.getElementById("js-side-nav").style.width = '250px';
+  document.getElementById("js-main").style.marginLeft = '250px';
+}
+
+function closeNav(){
+  document.getElementById("js-side-nav").style.width = '0px';
+  document.getElementById("js-main").style.marginLeft = '0px';
+}
